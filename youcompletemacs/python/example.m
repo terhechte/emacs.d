7522d4fb731cbd1cb8362f8770534479818ea155
Benedikt Terhechte <terhechte@gmail.com>
2013-05-31 14:12:09 +0200

    half-working, tough stuff

---
+++ youcompletemacs/python/example.m
@@ -0,0 +1,27 @@
+#import <UIKit/UIKit.h>
+
+struct tpx {
+  int xp;
+  int yp;
+};
+
+@interface abc : NSObject
+@property (nonatomic, assign) bool test1;
+@property (nonatomic, assign) bool test2;
+@done
+
+@implementation abc
+@synthesize test1, test2;
+@end
+
+void main() {
+
+  UIViewController *uvx = [[UIViewController alloc] init];
+  uvx. 
+
+  abc *testThing = [[abc alloc] init];
+  [abc  
+  
+  NSString *s = @"";
+  NSLog(@"%@", s. );
+}
