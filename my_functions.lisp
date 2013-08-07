ffc08ed6461c09161170546a4d2cadb69efdac54
Benedikt Terhechte <terhechte@gmail.com>
2013-08-07 08:45:12 +0200

    added custom functions

---
+++ my_functions.lisp
@@ -0,0 +1,28 @@
+(defun create-column (name type)
+  (setq tp (split-string name "_"))
+  (setq oname (car (cdr tp)))
+  (setq vname (if oname oname name))
+  (setq vname2 (if oname name (format "ev_%s" name)))
+    (insert (format "  def %s = column[%s] (\"%s\")\n" vname type vname2))
+  )
+
+(defun create-icolumn (name)
+  "Insert a complete column description"
+  (interactive "Mname:")
+  (create-column name "Int")
+  )
+(global-set-key (kbd "C-c C-1") 'create-icolumn)
+
+(defun create-scolumn (name)
+  "Insert a complete sttring comlumn description"
+  (interactive "Mname:")
+  (create-column name "String")
+  )
+(global-set-key (kbd "C-c C-2") 'create-scolumn)
+
+(defun create-dcolumn (name)
+  "Insert a complete double column description"
+  (interactive "Mname:")
+  (create-column name "Double")
+  )
+(global-set-key (kbd "C-c C-3") 'create-dcolumn)
