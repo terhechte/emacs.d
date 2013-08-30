f67d8ffee11d20e017b487b239c9759c14ca10e0
Benedikt Terhechte <terhechte@gmail.com>
2013-08-30 22:31:32 +0200

    more functions

--- my_functions.lisp
+++ my_functions.lisp
@@ -27,16 +27,15 @@
   )
 (global-set-key (kbd "C-c C-3") 'create-dcolumn)
 
-
 ; retrieve url, and switch to buffer
 (defun get-buf-url (url)
-  (interactive)
+  (interactive "surl to fetch:")
   (url-retrieve url
                 (lambda (status) (switch-to-buffer (current-buffer)))))
 
 (require 'request)
 (defun get-msg-url (url)
-  (interactive)
+  (interactive "surl to fetch:")
   (request url
            :success (function*
                      (lambda (&key data &allow-other-keys)
@@ -59,3 +58,100 @@
     )
   )
 
+; quickly restart the audition
+(defun sbt-startup-done ()
+  (goto-char (point-max))
+  (if (string-equal (current-word) ">")
+      t
+    nil))
+
+(defun audition-restart ()
+  (interactive)
+  (let ((buf (get-buffer "sbt")))
+    (with-current-buffer buf
+        (set-buffer buf)
+        (evil-insert 1)
+        (term-send-raw-string (kbd "C-c RET"))
+        (term-send-raw-string (kbd "C-c RET"))
+        (term-send-raw-string "./sbt\n")
+        (sleep-for 1.0)
+        (while (not (sbt-startup-done))
+          (sleep-for 0.1))
+        (term-send-raw-string "container:start\n")
+        (sleep-for 1.0)
+        (while (not (sbt-startup-done))
+          (sleep-for 0.1))
+        ; and set-up the audition
+        (setup-audition)
+      )
+  ))
+;(audition-restart)
+
+(defun audition-compile ()
+  (interactive)
+  (let ((buf (get-buffer "sbt")))
+    (with-current-buffer buf
+      (set-buffer buf)
+      (evil-insert 1)
+      (term-send-raw-string "compile\n")
+      (while (not (sbt-startup-done))
+        (sleep-for 0.1))
+      )))
+(audition-compile)
+
+
+; enclose word in tag
+(defun enclose-word-in-tag (tag)
+  (interactive "stag:")
+  (let (bds p1 p2 inputStr resultStr)
+
+    ;; get boundary
+    (if (region-active-p)
+        (setq bds (cons (region-beginning) (region-end)))
+      (setq bds (bounds-of-thing-at-point 'word)))
+    (setq p1 (car bds))
+    (setq p2 (cdr bds))
+
+    ;; grab the string
+    (setq inputStr (buffer-substring-no-properties p1 p2))
+    (setq resultStr (concat "<" tag ">" inputStr "</" tag ">"))
+    (delete-region p1 p2)
+    (insert resultStr)
+    ))
+
+(defun rr ()
+  (let ((buf (get-buffer "*ansi-term*")))
+    (with-current-buffer buf
+      ;(term-down 100)
+      (scroll-down)
+      )))
+(rr)
+(defun run-script ()
+  (interactive)
+  (let ((fil (buffer-file-name))
+        (dir default-directory)
+        (buf (get-buffer "*ansi-term*")))
+    (with-current-buffer buf
+      (evil-insert 1)
+      (if (not (string-equal dir default-directory))
+          (progn
+            (term-send-raw-string (concat "cd " dir))
+            (term-send-raw-string (kbd "RET"))))
+      (term-send-raw-string (concat "python " fil))
+      (term-send-raw-string (kbd "RET"))
+      (evil-normal-state)
+      )))
+;(global-set-key (kbd "C-c"))
+(evil-ex-define-cmd "R" 'run-script)
+
+(defun scroll-script ()
+  (let ((buf (get-buffer "*ansi-term*")))
+    (with-current-buffer buf
+      (evil-insert 1)
+      (term-send-raw-string (kbd "RET"))
+      (term-send-raw-string (kbd "RET"))
+      (evil-normal-state)
+      )))
+(scroll-script)
+
+(evil-ex-define-cmd "Rx" 'scroll-script)
