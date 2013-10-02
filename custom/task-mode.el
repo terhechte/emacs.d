9e702127d705cd7103fd3e64afdcf66577e0920f
Benedikt Terhechte <terhechte@gmail.com>
2013-10-02 09:50:34 +0200

    extended task mode

--- custom/task-mode.el
+++ custom/task-mode.el
@@ -1,5 +1,8 @@
-; http://www.emacswiki.org/emacs/GenericMode
 ; Try adding my own minor mode for taskpaper, that really just offers syntax highlighting
+; This mode currently utilizes evil functionality, since I use emacs with evil. It would be trivial to make
+; this a full major mode that does not depend upon evil, but this is more or less a small project for me
+; to create the perfect todo mode, and to learn more emacs & more elisp
+; http://www.emacswiki.org/emacs/GenericMode
 (require 'generic-x)
 (define-generic-mode
     'task-mode ;name of the mode
@@ -11,9 +14,10 @@
         (lambda () (highlight-lines-matching-regexp "\\@later" "hi-blue-b"))
         (lambda () (highlight-lines-matching-regexp "\\@testing" "hi-yellow-b"))
         (lambda () (highlight-lines-matching-regexp "\\@important" "hi-red-b"))
+        (lambda () (highlight-lines-matching-regexp "\\:$" "hi-pink"))
+        (lambda () (highlight-lines-matching-regexp "\\:$" "hi-blue-b"))
         (lambda () (highlight-lines-matching-regexp "\\@non-happen" "hi-gray-b")))
   "A mode for task files")
-  
 
 (defun task-mode-archive-done ()
   ; Archive all the done tasks at the bottom of the page
@@ -40,9 +44,9 @@
 
 
 (defun task-mode-todo-task ()
-  ; add '@done to the end of the line
+  ; add '@done to the end of the line, also add the time when it was done
   (interactive)
-  (task-mode-insert-at-end "@done"))
+  (task-mode-insert-at-end (format "@done (@%s)" (current-time-string))))
 
 (defun task-mode-testing-task ()
   (interactive)
@@ -61,9 +65,32 @@
   (task-mode-insert-at-end "@non-happen"))
 
 (defun task-mode-insert-at-end (text)
-  (save-excursion
+  ; mark function
+  (defun mark-with-text (text)
     (goto-char (line-end-position))
-    (insert (format " %s" text))))
+    (insert (format " %s" text)))
+  (save-excursion
+    ; if we have a region active, go through all lines                                       
+    (if (region-active-p)
+        (let ((org-region (region-end))
+              (start-point (if (< (region-end) (region-beginning))
+                               (region-end)
+                             (region-beginning))))
+          (goto-char start-point)
+          (while (<= (point) org-region)
+            (message "mark-active")
+            (mark-with-text text)
+            (goto-char (line-beginning-position))
+            (forward-line 1)))
+      ; Else, just mark the one line
+      (mark-with-text text))))
 
+(defun task-mode-new-todo (task)
+  ; Add a new todo
+  (interactive "sEnter Task: ")
+    (evil-normal-state)
+    (evil-insert-newline-below)
+    (insert (format "- %s (%s)" task (current-time-string)))
+    (goto-char (line-beginning-position)))
 
 (provide 'task-mode)
